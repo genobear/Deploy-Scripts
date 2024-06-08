fc794f882d1cec0e4bbf37bdaceae684475ea8a4
genobear <33195938+genobear@users.noreply.github.com>
2024-06-08 18:52:01 +0100

    cwo-gn

---
+++ CWO-GN/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+BRANCH="main"  # or the branch you want to deploy
+REPO_URL="https://github.com/genobear/CWO-to-GN-Permit-Ingestion.git"
+
+DEPLOY_DIR="/usr/local/apps/CWO-GN-sync"
+SERVICE_NAME="FWO-GN-sync"
+SCRIPT_NAME="main.py"
+
+# make directory
+mkdir -p $DEPLOY_DIR
+mkdir -p $DEPLOY_DIR/env
+mkdir -p $DEPLOY_DIR/logs
+mkdir -p $DEPLOY_DIR/deploy
+
+
+# Step 1: Clone the repository
+git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
+
+# Step 2: Set up a Python virtual environment and install dependencies
+cd $DEPLOY_DIR && python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
+
+# Step 3: Create a systemd service file
+SERVICE_FILE=$(cat <<EOF
+[Unit]
+Description=CWO Live Purchases to Group Nexus Permit Ingestion
+After=network.target
+
+[Service]
+User=ubuntu
+Group=ubuntu
+WorkingDirectory=$DEPLOY_DIR
+Environment="PATH=$DEPLOY_DIR/venv/bin"
+ExecStart=$DEPLOY_DIR/venv/bin/python $DEPLOY_DIR/$SCRIPT_NAME
+
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+)
+
+# Step 4: Transfer the service file to the remote server
+echo "$SERVICE_FILE" | sudo tee /etc/systemd/system/$SERVICE_NAME.service
+
+# Step 5: Reload systemd, enable and start the service
+sudo systemctl daemon-reload && sudo systemctl enable $SERVICE_NAME.service && sudo systemctl start $SERVICE_NAME.service
