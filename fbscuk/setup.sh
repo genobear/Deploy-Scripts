cfbca419a806e00ab4cf6f731de7ce0c4ea3638a
genobear <33195938+genobear@users.noreply.github.com>
2024-08-11 12:54:47 +0100

    Update setup.sh

--- fbscuk/setup.sh
+++ fbscuk/setup.sh
@@ -18,10 +18,10 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y python3 python3-pip python3-venv git nginx curl
 
 # Step 2: Clone the repository
-echo "Creating deployment directory..."
-mkdir -p $DEPLOY_DIR
-echo "Cloning the repository..."
-git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
+# echo "Creating deployment directory..."
+# mkdir -p $DEPLOY_DIR
+# echo "Cloning the repository..."
+# git clone -b $BRANCH $REPO_URL $DEPLOY_DIR
 
 # Step 3: Set up a Python virtual environment and install dependencies
 echo "Setting up a Python virtual environment and installing dependencies..."
